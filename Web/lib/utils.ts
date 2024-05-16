45167a35011b20f2f2e2e5732e47312c79a361ed
Wulabalabo <cyberchenjw@gmail.com>
2024-05-16 20:18:35 +0800

    [feat] add uint8Array2String fun in web/lib/utils

--- Web/lib/utils.ts
+++ Web/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const unit8Array2String = (uint8Array:Uint8Array):string =>{
+    const decoder = new TextDecoder();
+    return decoder.decode(uint8Array);
+}
