cc1242332a9f6ea05cf5640ecd2136559aeb2b35
Wulabalabo <cyberchenjw@gmail.com>
2024-05-16 23:00:39 +0800

    [feat] add stringToUint8Array function

--- Web/lib/utils.ts
+++ Web/lib/utils.ts
@@ -9,3 +9,10 @@ export const unit8Array2String = (uint8Array:Uint8Array):string =>{
     const decoder = new TextDecoder();
     return decoder.decode(uint8Array);
 }
+
+export const stringToUint8Array = (str: string): Uint8Array => {
+  const encoder = new TextEncoder();
+  return encoder.encode(str);
+};
+
+
