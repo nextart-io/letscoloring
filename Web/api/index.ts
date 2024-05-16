75f0e2b7d0b37f3757f7e22ede211cf46c9057ca
Wulabalabo <cyberchenjw@gmail.com>
2024-05-16 16:05:46 +0800

    [fix] FillGrid

--- Web/api/index.ts
+++ Web/api/index.ts
@@ -1,11 +1,11 @@
 import { bcs } from "@mysten/bcs";
-import { GetObjectParams, SuiClient } from "@mysten/sui.js/client";
+import { GetObjectParams, SuiClient, SuiObjectResponse } from "@mysten/sui.js/client";
 import { TransactionBlock } from "@mysten/sui.js/transactions"
-import { truncate } from "lodash";
 
 const Package = "0x95df9c6f7fbb4e9ce777aaddb6f2d832a7fceffa620bd26d8a577177599730a6";
 const Gm = "0x2d555a1ce4d5694f3a70b29262ed8b0cd28bbc2d0063bf0fc9a91ed2c5626852";
 const Rt = "0x20cc25fa22841cc358124a3ab2e7a7a4cd9678e5b2310a204d9c34306863a281"
+
 
 /*public fun start_new_game(
     gm: &mut GameManager, 
@@ -42,13 +42,14 @@ export const StartNewGame = (payment: string, rows: string, cols: string, color:
         ctx:&mut TxContext
     )
 */
-export const FillGrid = (game:string,token:string,row:string,col:string,new_color:string)=>{
+export const FillGrid = (game:string,payment:string,row:string,col:string,new_color:string)=>{
     const txb = new TransactionBlock();
+    let [coin] = txb.splitCoins(txb.gas,[payment])
     txb.moveCall({
         target:`${Package}::coloring::fill_grid`,
         arguments:[
             txb.object(`${game}`),
-            txb.object(`${token}`),
+            txb.object(`${coin}`),
             txb.pure.u64(row),
             txb.pure.u64(col),
             txb.pure(bcs.string().serialize(new_color)),
@@ -63,16 +64,38 @@ public fun settlement(
         game: &mut Game, 
         ctx: &mut TxContext
 */
-export const Settlement = ()=>{
+export const Settlement = (game:string)=>{
+    const txb = new TransactionBlock();
+    txb.moveCall({
+        target:`${Package}::coloring::settlement`,
+        arguments:[
+            txb.object(`${game}`)
+        ]
+    })
 
+    return txb;
 }
 
-export const getGameInfo = async (client:SuiClient,id:string) => {
+/*
+    public struct Game has key, store {
+        id: UID,
+        payment: u64, 1_000_000_000 = 1SUI
+        rows: u64,
+        cols: u64,
+        cnt: u64,
+        colors:vector<vector<u8>>,
+        grids: vector<vector<Grid>>,
+        total_reward: Balance<SUI>,
+        grid_player: vector<vector<address>>,
+    }
+
+*/
+export const getGameInfo= async (client:SuiClient,game_id:string):Promise<SuiObjectResponse> => {
     const params:GetObjectParams = {
-        id: id,
+        id: game_id,
         options:{
             showContent:true,
         }
     }
-    await client.getObject(params);  
+    return await client.getObject(params);  
 }
